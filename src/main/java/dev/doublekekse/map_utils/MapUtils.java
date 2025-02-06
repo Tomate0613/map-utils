61f631ee823649fdad8ad202b966aa7c015eaa86
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-02-06 21:42:54 +0100

    Register commands again (oops)

--- src/main/java/dev/doublekekse/map_utils/MapUtils.java
+++ src/main/java/dev/doublekekse/map_utils/MapUtils.java
@@ -2,17 +2,13 @@ package dev.doublekekse.map_utils;
 
 import dev.doublekekse.map_utils.data.MapUtilsSavedData;
 import dev.doublekekse.map_utils.packet.*;
-import dev.doublekekse.map_utils.registry.MapUtilsBlockEntities;
-import dev.doublekekse.map_utils.registry.MapUtilsBlocks;
-import dev.doublekekse.map_utils.registry.MapUtilsCreativeTabs;
-import dev.doublekekse.map_utils.registry.MapUtilsPackets;
+import dev.doublekekse.map_utils.registry.*;
 import dev.doublekekse.map_utils.state.CameraOverrideState;
 import dev.doublekekse.map_utils.timer.CommandCallback;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.SharedConstants;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.timers.TimerCallbacks;
@@ -44,9 +40,9 @@ public class MapUtils implements ModInitializer {
 
         MapUtilsBlocks.register();
         MapUtilsBlockEntities.register();
-        MapUtilsBlockEntities.register();
         MapUtilsCreativeTabs.register();
         MapUtilsPackets.register();
+        MapUtilsCommands.register();
 
         ServerPlayConnectionEvents.JOIN.register((listener, packetSender, server) -> syncData(packetSender, server));
     }
